fba1e34f4af6ee3f5c91c3ea3c3165108c95b475
Hans Wong <37435886+gt-hans@users.noreply.github.com>
2023-06-04 15:53:23 +0000

    CSP wording changes

--- app/frontend/src/pages/layout/Layout.tsx
+++ app/frontend/src/pages/layout/Layout.tsx
@@ -8,7 +8,7 @@ const Layout = () => {
             <header className={styles.header} role={"banner"}>
                 <div className={styles.headerContainer}>
                     <Link to="/" className={styles.headerTitleContainer}>
-                        <h3 className={styles.headerTitle}>GPT + Enterprise data | Sample</h3>
+                        <h3 className={styles.headerTitle}>GPT + ChainStorePlus | Sample</h3>
                     </Link>
                     <nav>
                         <ul className={styles.headerNavList}>
@@ -35,7 +35,7 @@ const Layout = () => {
                             </li>
                         </ul>
                     </nav>
-                    <h4 className={styles.headerRightText}>ChainStorePlus POS & Backend Manual</h4>
+                    <h4 className={styles.headerRightText}>ChainStorePlus POS & Backoffice</h4>
                 </div>
             </header>
 
